8ebd6afa0ad5ca359f4d4772a2fa16da917a183c
Akashkr28 <email.akash2010@gmail.com>
2025-11-18 21:54:50 +0530

    Created vite based UI

---
+++ backend/app.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from code_graph import graph as code_graph
+from fastapi.middleware.cors import CORSMiddleware
+from graph import graph
+
+app = FastAPI()
+
+class QueryRequest(BaseModel):
+    query: str
+
+@app.post("/api/ask")
+def ask_question(request: QueryRequest):
+    state = {
+        "query": request.query,
+        "llm_result": None
+    }
+    result = graph.invoke(state)
+    return {"response": result["llm_result"]}
+
+@app.post("/api/code")
+def ask_code(request: QueryRequest):
+    state = {
+        "user_query": request.query,
+        "llm_result": None,
+        "accuracy_percentage": None,
+        "is_coding_question": False
+    }
+    result = code_graph.invoke(state)
+    return result
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/api/test")
+def test_api():
+    return {"message": "API is working!"}
