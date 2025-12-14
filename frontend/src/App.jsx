56bce0079daf8d536f436338afe8ed1f058630ea
Akashkr28 <email.akash2010@gmail.com>
2025-12-14 11:31:42 +0530

    Added Routing Infrastructure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import ChatPage from './pages/ChatPage';
 
 function App() {
   const isAuthenticated = false; // Replace with real authentication logic
@@ -6,7 +8,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+
+        {/* If Authenticated show chat page, else redirect to landing page */}
+        <Route
+          path='/chat'
+          element={isAuthenticated ? <ChatPage/> : <Navigate to='/' />}
+        />
       </Routes>
     </BrowserRouter>
   )
 }
+
+export default App
